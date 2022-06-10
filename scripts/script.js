d7f06d10120a22d2b511e244a7ffb183af37986f
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-06-10 15:34:07 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,64 @@ const operationMap = new Map([
   ["/", divide]
 ]);
 
-const calculate = function(num1, num2, symbol = "+") {
+// ---------- DOM OBJECTS ---------- //
+const display = document.querySelector(".display");
+const numberBtns = document.querySelectorAll(".number");
+const operationBtns = document.querySelectorAll(".operation");
+
+
+
+// ---------- BACKEND CALCULATIONS ---------- //
+let nums = [];
+let symbol = "+";
+let currentNum = "";
+
+const calculate = function(num1 = 0, num2 = 0, symbol) {
   const operation = operationMap.get(symbol);
   return operation(num1, num2);
-}
+};
 
-let nums = [];
-let symbol;
+const updateNumsAfterCalc = function(result, array) {
+  array.pop();
+  array[0] = result;
+};
+
+
+// ---------- CALCULATOR FUNCTIONALITY  ---------- //
+const updateDisplay = value => display.textContent = value;
+
+numberBtns.forEach(function(btn) {
+  btn.addEventListener("click", function() {
+
+    if (display.textContent.length === 8) return;
+    const numClicked = this.textContent;
+    currentNum += numClicked;
+    updateDisplay(currentNum);
+  });
+});
+
+for (const btn of operationBtns) {
+  if (btn.textContent === "=") {
+    btn.addEventListener("click", function() {
+      nums.push(Number(currentNum));
+      currentNum = "";
+      const result = calculate(...nums, symbol);
+      updateNumsAfterCalc(result, nums);
+      updateDisplay(result);
+    });
+  } else {
+    btn.addEventListener("click", function() {
+
+      nums.push(Number(currentNum));
+      currentNum = "";
+      const opClicked = this.textContent;
+
+      if (nums.length === 2) {
+        const result = calculate(...nums, symbol);
+        updateNumsAfterCalc(result, nums);
+        updateDisplay(result);
+      }
+      symbol = opClicked;
+    });
+  }
+}
